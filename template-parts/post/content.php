58b07582b1c3c0bf1dab91e76c94a2057b2ef82d
Sampad Debnath <sampad750@gmail.com>
2023-06-07 17:30:56 +0600

    add 5.3

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -1,14 +1,28 @@
-<div class="blog-entry ftco-animate d-md-flex">
-<a href="single.html" class="img img-2" style="background-image: url(images/image_1.jpg);"></a>
-<div class="text text-2 pl-md-4">
-	<h3 class="mb-2"><a href="single.html"><?php the_title();?></a></h3>
-	<div class="meta-wrap">
-		<p class="meta">
-			<span><i class="icon-calendar mr-2"></i>June 28, 2019</span>
-			<span><a href="single.html"><i class="icon-folder-o mr-2"></i>Travel</a></span>
-			<span><i class="icon-comment2 mr-2"></i>5 Comment</span>
-		</p>
+<div class="blog-entry d-md-flex"> <!-- "ftco-animate" use this class not showing this section-->
+	<a href="<?php the_permalink() ?>">
+		<?php the_post_thumbnail( 'thumbnail', array('class'=>'img img-2') )?>
+	</a>
+	<div class="text text-2 pl-md-4">
+		<h3 class="mb-2"><a href="<?php the_permalink() ?>"><?php the_title();?></a></h3>
+		<div class="meta-wrap">
+			<p class="meta">
+				<span><i class="icon-calendar mr-2"></i><?php the_time( 'M j, Y' ); ?></span>
+				<span>
+					<?php
+						foreach((get_the_category()) as $category) {
+							$postcat= $category->cat_ID;
+							$post_cat_link = get_category_link( $postcat );
+							$catname =$category->cat_name;
+							?>
+							<a href="<?php echo $post_cat_link; ?>"><i class="icon-folder-o mr-2"></i><?php echo $catname; ?></a>
+							<?php
+						}
+					?>
+				</span>
+				<span><i class="icon-comment2 mr-2"></i>5 Comment</span>
+			</p>
+		</div>
+		<p class="mb-4">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+		<p><a href="#" class="btn-custom">Read More <span class="ion-ios-arrow-forward"></span></a></p>
 	</div>
-	<p class="mb-4">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-	<p><a href="#" class="btn-custom">Read More <span class="ion-ios-arrow-forward"></span></a></p>
 </div>
